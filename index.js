15f2e6a4a3f7a3179da3fe6b3af43b5b7af2bec3
Rami <103028944+Rami24t@users.noreply.github.com>
2022-08-22 17:44:34 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,24 @@
 // Write your code here:
 
-
+function fibonacci(n) {
+    if (n === 0)
+        return [n];
+    if (n === 1)
+        return [0, 1]
+    if (typeof n == typeof 1) {
+        a = n;
+        n = new Array(a + 1);
+        n[n.length - 1] = a + 1;
+        n[0] = 0;
+        n[1] = 1;
+        return fibonacci(n)
+    }
+    if (typeof n == typeof [1]) {
+        for (i = 2; i < n.length; i++)
+            n[i] = (n[i - 1] + n[i - 2])
+        return n
+    }
+}
 
 console.log(fibonacci(0)); // [0]
 console.log(fibonacci(1)); // [ 0, 1 ]
